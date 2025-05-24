f7eebc3041f12c9bd0cd259d378d8ec56844dc1f
Talha Mahmood <rana.talha20003@gmail.com>
2025-05-25 00:34:35 +0200

    Doing the exercise from 1c to 6c

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import {OrderPage} from './pages/OrderPage'
 import {HomePage} from './pages/HomePage'
-import {Checkout} from './pages/CheckoutPage'
+import {Checkout} from './pages/checkout/CheckoutPage'
 import { Tracking } from './pages/TrackingPage'
 import './App.css'
 import { Routes,Route } from 'react-router'
