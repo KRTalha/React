d300ec08a6d8b948cb55be110c4312f851877a23
Talha Mahmood <rana.talha20003@gmail.com>
2025-05-24 23:52:21 +0200

    Now i have added finally the tracking page css and html and now can start work on ract now

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import {OrderPage} from './pages/OrderPage'
 import {HomePage} from './pages/HomePage'
 import {Checkout} from './pages/CheckoutPage'
+import { Tracking } from './pages/TrackingPage'
 import './App.css'
 import { Routes,Route } from 'react-router'
 
@@ -14,6 +15,7 @@ function App() {
       <Route index element={<HomePage />} />
       <Route path="checkout" element={<Checkout />} />
       <Route path="orders" element={<OrderPage />} />
+      <Route path="tracking" element ={<Tracking />} />
 
     </Routes>
       
