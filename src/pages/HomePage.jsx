dbcf9068ccc19fddeccda3693c532f5694efc396
Talha Mahmood <rana.talha20003@gmail.com>
2025-05-25 19:32:38 +0200

    i am fetching the data from the backend

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -3,7 +3,16 @@ import './HomePage.css'
 import {products} from '../../starting-code/data/products'
 
 export function HomePage(){
+  fetch('http://localhost:3000/api/products')
+    .then((response)=>{
+      return response.json()
+    }).then((data)=>{
+      console.log(data);
+      
+    })
 
+
+  
   return (
     <>
     <title>Ecommerce Project</title>
